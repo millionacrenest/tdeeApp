b46fe4b8d221ac4891ef92ac5ab4a28a36e4a590
Allison McEntire <amcentire@apple.com>
2020-11-16 07:02:49 -0800

    formated data on run detail view

--- tdeeApp/Runs/RunsDetailView.swift
+++ tdeeApp/Runs/RunsDetailView.swift
@@ -13,14 +13,37 @@ struct RunsDetailView: View {
     
     var workoutItem: HKWorkout
     
+
+    @State private var intervalToShow: String?
+    @State private var distanceToShow: String?
+    @State private var caloriesBurned: String?
+   
+    
     var body: some View {
-        VStack {
-            Text("Run completed: \(workoutItem.endDate)")
-            Spacer()
-            Text("Total Distance: \(workoutItem.totalDistance!)")
-            Text("Duration: \(workoutItem.duration)")
-            Text("Calories Burned: \(workoutItem.totalEnergyBurned!)")
+        ScrollView {
+            VStack {
+                Text("Run completed in \(intervalToShow ?? "")")
+                Text("Run distance: \(distanceToShow ?? "") miles")
+                Text("Run calories: \(caloriesBurned ?? "")")
+                
+            }.onAppear {
+                intervalToShow = formatRunTime(interval: workoutItem.duration)
+                distanceToShow = String(format: "%.0f", workoutItem.totalDistance?.doubleValue(for: HKUnit.mile()) ?? 0)
+                caloriesBurned = String(format: "%.0f", workoutItem.totalEnergyBurned?.doubleValue(for: HKUnit.kilocalorie()) ?? 0)
+            }
         }
     }
+    
+    func formatRunTime(interval: TimeInterval) -> String {
+        
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.unitsStyle = .abbreviated
+        
+        
+        return formatter.string(from: TimeInterval(interval))!
+    }
+    
+    
 }
 
